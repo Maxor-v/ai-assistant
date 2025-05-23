13e0bb18278944791a491d222b39fbe0e9562801
Максим <maxim.v.orlov@yandex.ru>
2025-05-23 17:35:11 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# API Keys и токены
+API_KEY_WAZZUP = "вставьте свой id"
+CHANNEL_ID = "вставьте свой номер канала whazzapp"
+API_KEY_OPENROUTER = "вставьте свой id нейросети"
+MODEL = "модель нейросети"
+
+MAX_HISTORY = 50
+SESSION_TTL = 1800  # 30 минут в секундах
